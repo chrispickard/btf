4c913828eacb17130ebfeae0e13f8e39ea1eda5e
Chris Pickard <chrispickard9@gmail.com>
2021-11-29 19:39:28 -0500

    added a flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,15 @@
 {
-  description = "A very basic flake";
+  description = "A simple, keyboard driven app switcher/launcher for x11";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
 
-    packages.x86_64-linux.btf = import ./default.nix;
+      packages.x86_64-linux.btf = (pkgs.callPackage ./default.nix { });
 
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.btf;
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.btf;
 
-  };
+    };
 }
